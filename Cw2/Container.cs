ae4f4f07f86f5b61ed3b39ef45d0e243d0c98f1d
s20209 <s20209@pjwstk.edu.pl>
2025-03-16 09:32:18 +0100

    commit

---
+++ Cw2/Container.cs
@@ -0,0 +1,28 @@
+﻿namespace Cw2;
+
+public class Container
+{
+    private double payloadWeight, maxLoadWeight, height, selfWeight, depth;
+    private ContainerType type;
+    private static int counter = 0;
+    private int id = 0;
+    private string serialNumber;
+
+    public Container(double payloadWeight, double maxLoadWeight, 
+        double height, double selfWeight, double depth)
+    {
+        this.payloadWeight = payloadWeight;
+        this.maxLoadWeight = maxLoadWeight;
+        this.height = height;
+        this.selfWeight = selfWeight;
+        this.depth = depth;
+        id = ++counter;
+    }
+
+    public enum ContainerType
+    {
+        L,
+        G,
+        C
+    }
+}
